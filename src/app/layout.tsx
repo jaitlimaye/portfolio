c996bf2453d7b613924693ce826d00cd8639c4a5
jaitlimaye <jaitlimaye@gmail.com>
2026-03-19 15:58:04 +0530

    feat analytics -added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className + " bg-gray-950 text-white"}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
